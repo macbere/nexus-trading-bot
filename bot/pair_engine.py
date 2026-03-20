8c31e55733c7b8a3a1995266eb7c77fe71f0fc3b
MacDonald Berei <macdonaldbereiweriso@gmail.com>
2026-03-20 09:33:48 +0000

    Fix BOTH lines 156 and 157 - cooldown logger syntax

--- bot/pair_engine.py
+++ bot/pair_engine.py
@@ -154,7 +154,7 @@ class PairEngine:
             minutes_since = (time.time() - self.last_trade_time[symbol]) / 60
             if minutes_since < self.cooldown_minutes:
                 logger.info("[PairEngine] {} in cooldown ({}m remaining)".format(symbol, remaining))
-                logger.info("[PairEngine] {} in cooldown ({}m remaining)".format(symbol, remaining))                return False
+                logger.info("[PairEngine] {} in cooldown ({}m remaining)".format(symbol, remaining))
         return True
 
     def _record_trade(self, symbol):
